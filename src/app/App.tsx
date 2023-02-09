7415b96be0538ad3c2a195a6ddefa2a25ab21a14
ElKlaus <klaus15rus@gmail.com>
2023-02-09 20:18:06 +0300

    Production-project 18: configuration eslint, fix error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,25 @@
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { AppRouter } from './providers/router';
+import { AppRouter } from 'app/providers/router';
 import './styles/index.scss';
-const App = () => {
+
+function App() {
     const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
-                <div className='content-page'>
+                <div className="content-page">
                     <Sidebar />
                     <AppRouter />
-                </div>                
+                </div>
             </Suspense>
         </div>
     );
-};
+}
 
 export default App;
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,29 +1,27 @@
-import { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { routeConfig } from 'shared/config/routerConfig/routerConfig'
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routerConfig/routerConfig';
 
-const AppRouter = () => {
-  return (
+const AppRouter = () => (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-              {Object.values(routeConfig).map(({element, path}) => (
-                <Route
-                  key={path}
-                  path={path}
-                  element={(
-                    <Suspense fallback={<div>Loading</div>}>
-                      <div className='page-wrapper'>
-                        {element}
-                      </div>
-                    </Suspense>
-                  )}
-                />
-              ))}
-          </Routes>
-      </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={(
+                            <Suspense fallback={<div>Loading</div>}>
+                                <div className="page-wrapper">
+                                    {element}
+                                </div>
+                            </Suspense>
+                        )}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
     </div>
-  )
-}
+);
 
-export default AppRouter
+export default AppRouter;
