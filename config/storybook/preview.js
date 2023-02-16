9796965d50f5690aed814d96ff264d024c4297ef
ElKlaus <klaus15rus@gmail.com>
2023-02-16 12:35:44 +0300

    Production-project 27: add check for github actions fix10

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,8 @@
-import { addDecorator } from "@storybook/react";
-import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
-import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
-import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
-
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -14,7 +13,6 @@ export const parameters = {
         },
     },
 };
-
 
 addDecorator(StyleDecorator);
 addDecorator(RouterDecorator);
