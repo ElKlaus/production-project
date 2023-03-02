b38c958b74b8029459b08eed89f308c61fb39b8f
ElKlaus <klaus15rus@gmail.com>
2023-03-02 21:25:51 +0300

    Production-project 40: Fix types for strict mode

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { ProfileShema } from '../types/profile';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
+import { Profile, ProfileShema } from '../types/profile';
 
 const initialState: ProfileShema = {
     readonly: true,
@@ -12,6 +13,24 @@ export const profileSlice = createSlice({
     name: 'profile',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProfileData.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchProfileData.fulfilled, (
+                state,
+                action: PayloadAction<Profile>,
+            ) => {
+                state.isLoading = true;
+                state.data = action.payload;
+            })
+            .addCase(fetchProfileData.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
 });
 
 // Action creators are generated for each case reducer function
