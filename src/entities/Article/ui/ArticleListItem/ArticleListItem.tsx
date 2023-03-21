e8b4be25cfe7bf1c9c71c105a766823ba9eb89a2
ElKlaus <klaus15rus@gmail.com>
2023-03-21 20:07:01 +0300

    Production-project 54: create components ArticleList and ArticleItem

---
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Text } from 'shared/ui/Text/Text';
+import { Icon } from 'shared/ui/Icon/Icon';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import { Card } from 'shared/ui/Card/Card';
+import cls from './ArticleListItem.module.scss';
+import { Article, ArticleView } from '../../model/types/article';
+
+interface ArticleListItemProps {
+    className?: string;
+    article: Article;
+    view: ArticleView;
+}
+
+export const ArticleListItem = memo((props: ArticleListItemProps) => {
+    const { className, article, view } = props;
+    const { t } = useTranslation();
+
+    if (view === ArticleView.BIG) {
+        return (
+            <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+                {article.title}
+            </div>
+        );
+    }
+
+    return (
+        <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+            <Card className={cls.card}>
+                <div className={cls.imageWrapper}>
+                    <img src={article.img} alt={article.title} className={cls.img} />
+                    <Text text={article.createdAt} className={cls.date} />
+                </div>
+                <div className={cls.infoWrapper}>
+                    <Text text={article.type.join(', ')} className={cls.types} />
+                    <Text text={String(article.views)} className={cls.views} />
+                    <Icon Svg={EyeIcon} />
+                </div>
+                <Text text={article.title} className={cls.title} />
+            </Card>
+        </div>
+    );
+});
