614cbd7625b1ef5d53fa3462f26a5302bc01d818
ElKlaus <klaus15rus@gmail.com>
2023-04-03 21:15:50 +0300

    Production-project 59: create selectors

--- src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -3,10 +3,15 @@ import { memo, useMemo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Select, SelectOption } from 'shared/ui/Select/Select';
 import { ArticleSortField } from 'entities/Article/model/types/article';
+import { SortOrder } from 'shared/types';
 import cls from './ArticleSortSelector.module.scss';
 
 interface ArticleSortSelectorProps {
     className?: string;
+    sort: ArticleSortField;
+    order: SortOrder;
+    onChangeOrder: (newOrder: SortOrder) => void;
+    onChangeSort: (newSort: ArticleSortField) => void;
 }
 
 export const ArticleSortSelector = memo((props: ArticleSortSelectorProps) => {
--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -1,14 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ArticleView, ArticleViewSelector } from 'entities/Article';
-import { articlesPageActions } from 'pages/ArticlesPage/model/slices/articlesPageSlice';
+import {
+    ArticleSortField, ArticleSortSelector, ArticleView, ArticleViewSelector,
+} from 'entities/Article';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
-import { getArticlesPageView } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
 import { Select } from 'shared/ui/Select/Select';
 import { Card } from 'shared/ui/Card/Card';
 import { Input } from 'shared/ui/Input/Input';
+import { SortOrder } from 'shared/types';
+import { getArticlesPageView } from '../../model/selectors/articlesPageSelectors';
+import { articlesPageActions } from '../../model/slices/articlesPageSlice';
 import cls from './ArticlesPageFilters.module.scss';
 
 interface ArticlesPageFiltersProps {
@@ -20,13 +23,28 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
     const view = useSelector(getArticlesPageView);
+
     const onChangeView = useCallback((view: ArticleView) => {
         dispatch(articlesPageActions.setView(view));
+    }, [dispatch]);
+
+    const onChangeSort = useCallback((newSort: ArticleSortField) => {
+        dispatch(articlesPageActions.setSort(newSort));
+    }, [dispatch]);
+
+    const onChangeOrder = useCallback((newOrder: SortOrder) => {
+        dispatch(articlesPageActions.setView(newOrder));
     }, [dispatch]);
 
     return (
         <div className={classNames(cls.ArticlesPageFilters, {}, [className])}>
             <div className={cls.sortWrapper}>
+                <ArticleSortSelector
+                    order={}
+                    sort={}
+                    onChangeOrder={}
+                    onChangeSort={}
+                />
                 <ArticleViewSelector view={view} onViewClick={onChangeView} />
             </div>
             <Card className={cls.search}>
